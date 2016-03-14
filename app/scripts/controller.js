5b7385f84e071f9aff20c6222d1f71a0fbdc8800
Tyler <tyler@geekingdoing.com>
2016-03-14 10:51:17 -0400

    started map model

--- app/scripts/controller.js
+++ app/scripts/controller.js
@@ -8,64 +8,64 @@
   * Create the sim controller
   */
  function SimController() {
-     // create a data model that exposes parameters to smart sparrow
-     this.simModel = new SimModel(this, {
-         mastery: 'false',
-         numerator: 7,
-         denominator: 10,
-         firstQuestion: 0,
-         lastQuestion: 0,
-     });
+   // create a data model that exposes parameters to smart sparrow
+   this.simModel = new SimModel(this, {
+     mastery: 'false',
+     numerator: 7,
+     denominator: 10,
+     firstQuestion: 0,
+     lastQuestion: 0,
+   });
 
-     // expose model data to Smart Sparrow
-     pipit.CapiAdapter.expose('mastery', this.simModel);
-     pipit.CapiAdapter.expose('numerator', this.simModel);
-     pipit.CapiAdapter.expose('denominator', this.simModel);
-     pipit.CapiAdapter.expose('firstQuestion', this.simModel);
-     pipit.CapiAdapter.expose('lastQuestion', this.simModel);
+   // expose model data to Smart Sparrow
+   pipit.CapiAdapter.expose('mastery', this.simModel);
+   pipit.CapiAdapter.expose('numerator', this.simModel);
+   pipit.CapiAdapter.expose('denominator', this.simModel);
+   pipit.CapiAdapter.expose('firstQuestion', this.simModel);
+   pipit.CapiAdapter.expose('lastQuestion', this.simModel);
 
 
-     // let smart sparrow know that the sim is ready to accept values
-     pipit.Controller.notifyOnReady();
-     // create the view controller
-     this.simView = new SimView(this);
+   // let smart sparrow know that the sim is ready to accept values
+   pipit.Controller.notifyOnReady();
+   // create the view controller
+   this.simView = new SimView(this);
  }
 
  SimController.prototype.initializeController = function() {
-     // initialize the model
-     this.simModel.initializeModel();
-     // initialize the view
-     this.setupDisplay();
+   // initialize the model
+   this.simModel.initializeModel();
+   // initialize the view
+   this.setupDisplay();
  }
 
 
  SimController.prototype.setModelValue = function(_name, _newValue) {
-     this.simModel.set(_name, _newValue);
+   this.simModel.set(_name, _newValue);
  }
 
 
  SimController.prototype.getModelValue = function(_name) {
-     return this.simModel.get(_name);
+   return this.simModel.get(_name);
  }
 
 
  SimController.prototype.triggerCheck = function() {
-     pipit.Controller.triggerCheck();
+   pipit.Controller.triggerCheck();
  }
 
 
  SimController.prototype.setupDisplay = function() {
-     this.simModel.mapExpression.randomDatatypeExpression();
-     // choose a question randomly
-     var question = this.simModel.questionBank.chooseQuestion(this.getModelValue('firstQuestion'), this.getModelValue('lastQuestion'));
-     // store the answer(s) to the question we chose in the last step
-     this.simModel.questionBank.setAnswers(this.simModel.mapExpression);
-     // draw the results for the last five questions
-     this.simView.questionBankView.drawAnswerHistory(this.simModel.questionBank.answerHistory);
-     // draw the expressions on the screen
-     this.simView.datatypeView.drawDatatypeExpression(this.simModel.mapExpression);
-     // display the next question
-     this.simView.questionBankView.presentQuestion(question);
+   this.simModel.mapExpression.randomDatatypeExpression();
+   // choose a question randomly
+   var question = this.simModel.questionBank.chooseQuestion(this.getModelValue('firstQuestion'), this.getModelValue('lastQuestion'));
+   // store the answer(s) to the question we chose in the last step
+   this.simModel.questionBank.setAnswers(this.simModel.mapExpression);
+   // draw the results for the last five questions
+   this.simView.questionBankView.drawAnswerHistory(this.simModel.questionBank.answerHistory);
+   // draw the expressions on the screen
+   this.simView.datatypeView.drawDatatypeExpression(this.simModel.mapExpression);
+   // display the next question
+   this.simView.questionBankView.presentQuestion(question);
  }
 
  // Create a new Controller for sim
@@ -74,6 +74,6 @@
 
 
  $(document).ready(function() {
-     // let smart sparrow know that the sim is ready to accept values
-     //pipit.Controller.notifyOnReady();
+   // let smart sparrow know that the sim is ready to accept values
+   //pipit.Controller.notifyOnReady();
  });
--- app/scripts/mapmodel.js
+++ app/scripts/mapmodel.js
@@ -1,6 +1,6 @@
 /*
  * Tyler Deans
- * February 21, 2016
+ * March 14, 2016
  */
 
 
@@ -8,6 +8,93 @@
 function MapModel(_simModel) {
     // save a link to the model
     this.simModel = _simModel;
+}
+
+function getQuestionType() {
+  var randNum = getRandomInt(1, 5);
+  var type = "";
+  if (randNum === 1) {
+    type = "arithmetic";
+  } else if (randNum === 2) {
+    type = "string";
+  } else if (randNum === 3) {
+    type = "head";
+  } else {
+    type = "null";
+  }
+
+  return type;
+}
+
+function getNumOfElements() {
+  // generate a number between 3 and 9
+  return getRandomInt(3, 10);
+}
+
+function numberListGenerator() {
+  var numOfElements = getNumOfElements();
+  var list = [];
+  for (var i = 0; i < numOfElements; i++) {
+    list[i] = getRandomInt(0, 10);
+  }
+  return list;
+}
+
+// The function creates a list of lists.
+function doubleListGenerator() {
+  // these sublists should be no longer than 3 elements
+  // must compensate for this specification
+  var list1 = numberListGenerator();
+  var list2 = numberListGenerator();
+  var list3 = numberListGenerator();
+
+  var list = [list1, list2, list3];
+  return list;
+
+}
+function getRandomString(strList) {
+  var index = getRandomInt(0, strList.length);
+  return strList[index];
+}
+
+/*
+ * The string list generator is similar to the number list generator
+ * Except that the elements come from a large list of strings
+ * The string from that list is picked randomly (index is randomly chosen)
+ * Then the string is appended to the list
+ *
+ * A method to determine the question type (number or string)
+ * returns a string (number or string)
+ *
+ */
+function stringListGenerator() {
+  var numOfElements = getNumOfElements();
+  var stringList = ["soup", "dog", "orange", "park", "cat", "helps", "talks", "castle", "genius", "flaming"];
+  var list = [];
+  for (var i = 0; i < numOfElements; i++) {
+    list[i] = getRandomString(stringList);
+  }
+  return list;
+}
+
+function getMathAnswer(list, operator, ) {
+
+}
+
+function getNullAnswer() {
+
+}
+
+function getMathOperator() {
+  var operator = "";
+  var operatorType= getRandomInt(1, 3);
+  if (operatorType === 1) {
+    operator = "+";
+  } else {
+    operator = "*";
+  }
+
+  return operator;
 }
 
 MapModel.prototype.MapProblem = function() {
