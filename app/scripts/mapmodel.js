64a77ccfbeb34476870c8b168535b454e63a8bfc
starving-geek <cleancoder01@gmail.com>
2016-03-14 23:56:27 -0400

    updated mapmodel.js

--- app/scripts/mapmodel.js
+++ app/scripts/mapmodel.js
@@ -11,50 +11,53 @@ function MapModel(_simModel) {
 }
 
 function getQuestionType() {
-  var randNum = getRandomInt(1, 5);
-  var type = "";
-  if (randNum === 1) {
-    type = "arithmetic";
-  } else if (randNum === 2) {
-    type = "string";
-  } else if (randNum === 3) {
-    type = "head";
-  } else {
-    type = "null";
-  }
+    var randNum = getRandomInt(1, 5);
+    var type = "";
+    if (randNum === 1) {
+        type = "arithmetic";
+    } else if (randNum === 2) {
+        type = "string";
+    } else if (randNum === 3) {
+        type = "head";
+    } else {
+        type = "null";
+    }
 
-  return type;
+    return type;
 }
 
 function getNumOfElements() {
-  // generate a number between 3 and 9
-  return getRandomInt(3, 10);
+    // generate a number between 3 and 9
+    return getRandomInt(3, 10);
 }
 
-function numberListGenerator() {
-  var numOfElements = getNumOfElements();
-  var list = [];
-  for (var i = 0; i < numOfElements; i++) {
-    list[i] = getRandomInt(0, 10);
-  }
-  return list;
+// generates a list of numbers
+// the length of the list is based on the numElements parameter
+function numberListGenerator(numElements) {
+    var list = [];
+    for (var i = 0; i < numElements; i++) {
+        list[i] = getRandomInt(0, 10);
+    }
+    return list;
 }
 
 // The function creates a list of lists.
 function doubleListGenerator() {
-  // these sublists should be no longer than 3 elements
-  // must compensate for this specification
-  var list1 = numberListGenerator();
-  var list2 = numberListGenerator();
-  var list3 = numberListGenerator();
+    var numOfElements = getRandomInt(1, 4);
+    // each list is no longer that 3 elements
+    var list1 = numberListGenerator(numOfElements);
+    var list2 = numberListGenerator(numOfElements);
+    var list3 = numberListGenerator(numOfElements);
 
-  var list = [list1, list2, list3];
-  return list;
+    // creates a list of lists
+    var list = [list1, list2, list3];
+    return list;
 
 }
+
 function getRandomString(strList) {
-  var index = getRandomInt(0, strList.length);
-  return strList[index];
+    var index = getRandomInt(0, strList.length);
+    return strList[index];
 }
 
 /*
@@ -68,40 +71,90 @@ function getRandomString(strList) {
  *
  */
 function stringListGenerator() {
-  var numOfElements = getNumOfElements();
-  var stringList = ["soup", "dog", "orange", "park", "cat", "helps", "talks", "castle", "genius", "flaming"];
-  var list = [];
-  for (var i = 0; i < numOfElements; i++) {
-    list[i] = getRandomString(stringList);
-  }
-  return list;
+    var numOfElements = getNumOfElements();
+    var stringList = ["soup", "dog", "orange", "park", "cat", "helps", "talks", "castle", "genius", "flaming"];
+    var list = [];
+    for (var i = 0; i < numOfElements; i++) {
+        list[i] = getRandomString(stringList);
+    }
+    return list;
 }
 
-function getMathAnswer(list, operator, ) {
+function getMathAnswer(operator, list, yVal) {
+    if (operator === "+") {
+        for (var i = 0; i < list.length; i++) {
+            list[i] = list[i] + yVal;
+        }
+    } else {
+        for (var i = 0; i < list.length; i++) {
+            list[i] = list[i] * yVal;
+        }
+    }
+}
+
+function getStringAnswer(list) {
+    var stringSizeList = [];
+    for (var i = 0; i < list.length; i++) {
+        stringSizeList = list[i].length;
+    }
+
+    return stringSizeList;
 
 }
 
-function getNullAnswer() {
+function getAddOperand() {
+    // returns a value between 1 and 3
+    return getRandomInt(1, 4);
+}
 
+function getMultiplyOperand() {
+    // returns a value between 2 and 5
+    return getRandomInt(2, 6);
+}
+
+function getNullAnswer(list) {
+    for (var i = 0; i < list.length; i++) {
+        var listStr = list[i].toString();
+        if (list[i] === "[]") {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+function getHeadOfList(list) {
+    return list[0];
 }
 
 function getMathOperator() {
-  var operator = "";
-  var operatorType= getRandomInt(1, 3);
-  if (operatorType === 1) {
-    operator = "+";
-  } else {
-    operator = "*";
-  }
+    var operator = "";
+    var operatorType = getRandomInt(1, 3);
+    if (operatorType === 1) {
+        operator = "+";
+    } else {
+        operator = "*";
+    }
 
-  return operator;
+    return operator;
 }
 
-MapModel.prototype.MapProblem = function() {
+MapModel.prototype.evalMapExpression = function() {
     this.mapString = "fun map (f,xs) =\n";
     this.mapString += "     case xs of\n";
     this.mapString += "         [] => []\n";
     this.mapString += "     | first::rest => (f first)::(map(f, rest))\n";
+
+    var question = getQuestionType();
+    if (question === "arithmetic") {
+
+    } else if (question === "string") {
+
+    } else if (question === "head") {
+
+    } else {
+
+    }
 }
 
 MapModel.prototype.getMapExpression = function() {
